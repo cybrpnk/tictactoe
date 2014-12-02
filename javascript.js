fde677e8624cb4307aa73f5f43f62e5e832370dc
mustafahaleem <haleem.developer@gmail.com>
2014-12-02 02:35:28 -0500

    Feature Introduction
    Completed intermediate difficulty processing and integration.

--- javascript.js
+++ javascript.js
@@ -63,9 +63,6 @@ function contains(needle, haystack){
 		progress[occurrences].push(haystack[i]);
 	}
 	
-	//for debugging purposes, log the progress array
-	console.log(progress);
-	
 	//return the progress array at the end of the function
 	return progress;
 }
@@ -85,7 +82,7 @@ function endgame(draw){
 		$("h2").text("Human wins!");
 	}
 	//see if the ai array contains any winning sets
-	if(contains(aiMoves)[3] != 0){
+	if(contains(aiMoves)[3].length != 0){
 		gameover = true;
 		$("h1").text("Game over.");
 		$("h2").text("Computer wins!");
@@ -109,7 +106,7 @@ $("input[name=level]").change(function(){
 	level = Number($("input:radio[name=level]:checked").val());
 })
 
-//create function to generate random numbers between range
+//create function to generate random numbers through range
 function random(min, max) {
 	var number = Math.floor(Math.random() * (max + 1 - min)) + min;
 	//return the number as a string
@@ -128,6 +125,8 @@ function randMove(){
 	while($.inArray(move, gameMoves) > -1){
 		//if so, generate another random move and repeat
 		move = randMove();
+		
+		//for debugging purposes, log the move
 		console.log(move);
 	}
 	return move;
@@ -188,7 +187,60 @@ function intelligence(){
 			case 1:
 				//INTERMEDIATE
 				
+				//create variable to store the user's progress for the move
+				var progress = contains(userMoves)[2];
 				
+				//check if the user has double progress towards a winning set
+				if(progress.length != 0){
+					//create variable to store attempts at counterattacking
+					attempts = [];
+				
+					//while the number of attempts to launch a counterstrike are
+					//less than the number of available counterattack options
+					while(attempts.length < progress.length){
+						//create a random variable to be used as key for counterattack
+						var rand = random(1, progress.length) - 1 /*adjust for offset*/;
+						//create while loop to create valid random number
+						//(one that has not been previously attempted)
+						while($.inArray(rand, attempts) > -1){
+							rand = random(1, progress.length) - 1 ;
+						}
+					
+						//pick a double progress set to initiate a counterattack to
+						//note: this assumes that there are multiple double progress sets,
+						//and if there's only one, then that number is the one picked
+						//as dictated by the random function (see it for reference)
+						var counterAttack = progress[rand];
+					
+						//if the counterattack is available
+						if(contains(gameMoves, [counterAttack])[3].length == 0){
+							//create loop to go through the user moves
+							for(var i = 0; i < userMoves.length; i++){
+								//check to see if the move is in the counterattack set
+								var existence = $.inArray(userMoves[i], counterAttack);
+								//if it is, remove it from the the counterattack set
+								if(existence != -1) counterAttack.splice(existence, 1);
+							}
+					
+							//the move to be played is now at the 0th index of the counterattack
+							//array, as it is the only value left. Therefore, play the move.
+							playMove(2, counterAttack[0]);
+							
+							//break the while loop
+							break;
+						}
+						
+						//push attempt to attempt variable
+						attempts.push(rand);
+					}
+					
+					//after that, if the amount of attempts has met the amount of counterstrike
+					//options, then play a random move
+					if(attempts.length == progress.length) playMove(2, randMove());
+				}
+				
+				//if not, then find a random move and play it
+				else playMove(2, randMove());
 			
 				break;
 			case 2:
@@ -214,9 +266,11 @@ $(".button").mouseup(function(){
 	//get user's move
 	var move = $(this).attr("data-space");
 	
-	//if this move hasnt been played yet
+	//if this move hasnt been played yet, the game is not over,
+	//and the user isn't locked out
 	if($.inArray(move, gameMoves) == -1 && !gameover && !lock){
 		
+		//play the move for the user
 		playMove(1, move);
 		
 		//lock user interaction
